6549be00406f6e2b0086bf5fead8b3aeb91dc92f
Quill Cronwall <quilljazz@gmail.com>
2021-09-06 13:57:30 +0900

    testing jquery on add to cart button

--- assets/application.js
+++ assets/application.js
@@ -1,1 +1,26 @@
 // Put your application javascript here
+// Use the cart API to make the AJAX request?
+jQuery.post('/cart/add.js', {
+  items: [
+    {
+      quantity: 1,
+      id: 1,
+      properties: {
+        'cart_index': 'Should be number of items in cart'
+      }
+    }
+  ]
+});
+// const addItemToCart = (event) => {
+//   fetch("/cart/add.js", {
+//     method: "POST",
+//     body: JSON.stringify({ query: event.currentTarget.value })
+//   })
+//     .then(response => response.json())
+//     .then((data) => {
+//       console.log(data.hits); // Look at local_names.default
+//     });
+// };
+
+// const input = document.querySelector("#search");
+// input.addEventListener("keyup", searchAlgoliaPlaces);
