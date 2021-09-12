532beb55270e540aac0a8b2097a41f456e153835
Quill Cronwall <quilljazz@gmail.com>
2021-09-12 20:24:19 +0900

    change function names

--- assets/metafield.js
+++ assets/metafield.js
@@ -1,21 +1,21 @@
 // Put your application javascript here
 const axios = require('axios').default; // using axios for API calls
 
-async function axiosCheckValue() {
+async function CheckValue() {
   const { data: checkResponse } = await axios.get('https://aa429cff3713812e45b04ef7d83c3c30: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@teslazonda.myshopify.com/admin/api/2021-07/products/6958978400416/metafields.json');
   value = checkResponse['metafields'][0]['value'];
   idNumber = checkResponse['metafields'][0]['id'];
   return [value, idNumber]
 }
 // A funtion to delete metafields for bacon product for testing
-async function axiosDeleteMetafield() {
-  const metafieldIdToDeleteArray = await axiosCheckValue();
+async function deleteMetafield() {
+  const metafieldIdToDeleteArray = await CheckValue();
   deleteId = metafieldIdToDeleteArray[1]
   const { data: response } = await axios.delete(`https://aa429cff3713812e45b04ef7d83c3c30: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@teslazonda.myshopify.com/admin/api/2021-07/products/6958978400416/metafields/${deleteId}.json`) // chance number before .json to whatever the current id is.
   console.log(response)
   console.log('Metafield deleted.')
 }
-async function axiosNewMetafield() {
+async function newMetafield() {
   const { data: postResponse } = await axios.post('https://aa429cff3713812e45b04ef7d83c3c30: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@teslazonda.myshopify.com/admin/api/2021-07/products/6958978400416/metafields.json', {
   "metafield": {
     "namespace": "global",
@@ -29,20 +29,20 @@ let metafieldID = postResponse['metafield']['id']
 let metafieldValue = postResponse['metafield']['value']
 return [metafieldID, metafieldValue]
 }
-async function axiosGetMetafieldCount() {
+async function getMetafieldCount() {
   try {
     const { data: response } = await axios.get('https://aa429cff3713812e45b04ef7d83c3c30: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@teslazonda.myshopify.com/admin/api/2021-07/products/6958978400416/metafields/count.json') //use data destructuring to get data from the promise object
     console.log("This is the count of the metafields for Bacon product: ", response['count']);
     response['count'] //when the count of metafields is zero, create a new one
     if (response['count'] === 0) {
       console.log("Making POST request to create metafield because there are 0")
-      // Calling axiosNewMetafield creates new metafield
-      axiosNewMetafield();
+      // Calling newMetafield creates new metafield
+      newMetafield();
       console.log('New Metafield created:')
     } else {
       console.log("Making PUT request to update metafield value because metafield already exists");
       // Get the metafield id and value to use for the PUT request
-      let metafieldArray = await axiosCheckValue();
+      let metafieldArray = await CheckValue();
       // is an array [metafieldID, metafieldValue]
       console.log('This is the metafield ID and metafield value we using to update the metafield value:', metafieldArray)
       // axios metafield PUT to adds one to the metafield value
@@ -57,6 +57,8 @@ async function axiosGetMetafieldCount() {
       }
     })
     console.log(putResponse);
+    console.log('This is the updated value:', putResponse['metafield']['value']);
+
   }
 }
 
@@ -64,6 +66,6 @@ catch (error) {
   console.log(error);
 }
 }
-axiosGetMetafieldCount();
-// axiosDeleteMetafield();
-// Comment out axiosGetMetafieldCount() and use above function to delte metafield for testing
+getMetafieldCount();
+// deleteMetafield();
+// Comment out getMetafieldCount() and use above function to delte metafield for testing
