9ff4dc14603ec91ea2c089913519aa833270d601
Quill Cronwall <quilljazz@gmail.com>
2021-09-12 16:17:22 +0900

    fetch refactored, ready to integrate nested POST for create new metafields

--- assets/metafield.js
+++ assets/metafield.js
@@ -27,38 +27,36 @@ response:
   }
 }
 */
-// import fetch from 'node-fetch';
-// fetch('https://aa429cff3713812e45b04ef7d83c3c30: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@teslazonda.myshopify.com/admin/api/2021-07/metafiels/count.json', {
-//   method: 'GET',
-//   headers: {
-//     'Content-Type': 'application/json'
-//   }
-// })
-//   .then(response => {
-//     console.log(response)
-//     return response.json();
-//   })
-//   .catch((error) => {
-//     console.error('Error:', error);
-//   });
 const axios = require('axios').default; // with return {count: 0} if there are no metafields
-async function run() {
-  var options = {
-    method: 'GET',
-    url: 'https://aa429cff3713812e45b04ef7d83c3c30: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@teslazonda.myshopify.com/admin/api/2021-07/products/6958978400416/metafields/count.json',
-  };
-  axios(options)
-    .then(function (response) {
-      // handle success
-      console.log(response.data);
-      return response.data
-    })
-    .catch(function (error) {
-      // handle error
-      console.log(error.response.data);
-    })
+// axios.get('https://aa429cff3713812e45b04ef7d83c3c30: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@teslazonda.myshopify.com/admin/api/2021-07/products/6958978400416/metafields/count.json')
+//   .then(function (response) {
+//     // handle success
+//     console.log(response.data);
+//     return response.data
+//   })
+//   .catch(function (error) {
+//     // handle error
+//     console.log(error);
+//   })
+//   .then(function () {
+//     // always executed
+//   });
+async function axiosTest() { // perhaps have a nested async for POST
+  try {
+    const { data: response } = await axios.get('https://aa429cff3713812e45b04ef7d83c3c30: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@teslazonda.myshopify.com/admin/api/2021-07/products/6958978400416/metafields/count.json') //use data destructuring to get data from the promise object
+    console.log(response);
+    response['count'] //when the count of metafields is zero, create a new one
+    if (response['count'] === 0) {
+      // An Axios POST Here
+    }
+  }
+
+  catch (error) {
+    console.log(error);
+  }
 }
-run();
+axiosTest();
+// console.log(lst)
 // {
 //   "metafield": {
 //     "namespace": "inventory",
