4a82b29e7ae230e5531fa43d5cbead44e444cf6b
Quill Cronwall <quilljazz@gmail.com>
2021-09-12 14:12:48 +0900

    getting metafield count for product in store

--- assets/metafield.js
+++ assets/metafield.js
@@ -1,4 +1,5 @@
 // Put your application javascript here
+// require('dotenv').config();
 /*Retrieve a count of metafields that belong to a Product resource
 
 GET / admin / api / 2021 - 07 / products / 632910392 / metafields / count.json
@@ -20,23 +21,49 @@ response:
 {
   "metafield": {
     "namespace": "inventory",
-    "key": "warehouse",
-    "value": 25,
-    "type": "number_integer"
+    "key": "test",
+    "value": 0,
+    "type": "integer"
   }
 }
 */
-
-fetch('/admin/api/2021-07/products/6958978400416/metafields/count.json', {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(formData)
+// import fetch from 'node-fetch';
+// fetch('https://aa429cff3713812e45b04ef7d83c3c30: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@teslazonda.myshopify.com/admin/api/2021-07/metafiels/count.json', {
+//   method: 'GET',
+//   headers: {
+//     'Content-Type': 'application/json'
+//   }
+// })
+//   .then(response => {
+//     console.log(response)
+//     return response.json();
+//   })
+//   .catch((error) => {
+//     console.error('Error:', error);
+//   });
+const axios = require('axios').default; // with return {count: 0} if there are no metafields
+async function run() {
+  var options = {
+    method: 'GET',
+    url: 'https://aa429cff3713812e45b04ef7d83c3c30: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@teslazonda.myshopify.com/admin/api/2021-07/products/6958978400416/metafields/count.json',
+  };
+  axios(options)
+    .then(function (response) {
+      // handle success
+      console.log(response.data);
+      return response.data
     })
-    .then(response => {
-      return response.json();
+    .catch(function (error) {
+      // handle error
+      console.log(error.response.data);
     })
-    .catch((error) => {
-      console.error('Error:', error);
-    });
+}
+run();
+// {
+//   "metafield": {
+//     "namespace": "inventory",
+//       "key": "test",
+//         "value": 0,
+//           "type": "integer"
+//   }
+// }
